c90d14d93d1abe9c6f30b5656a8458f2777ec477
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2021-01-07 14:21:40 -0700

    completed all react tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -10,10 +10,10 @@ describe('About component', () => {
   it('renders', () => {
     render(<About />);
   });
+
   // second test to compare snapshots of the DOM node structure
   it('matches snapshot DOM node structure', () => {
     const { asFragment } = render(<About />);
     expect(asFragment()).toMatchSnapshot();
   })
-
-})
+});
